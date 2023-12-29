d545d50de6bb57f1814d92cbdca1598890832169
Alexei Belozertsev <belozertsev.alexei@gmail.com>
2023-12-29 08:29:16 -0800

    fix: return current selected

--- WellnessBingo/Menu/Menu.cs
+++ WellnessBingo/Menu/Menu.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,9 @@ namespace WellnessBingo
         private readonly string[] elements = { "December", "November", "October", "September", "July", "June", "National Wellness Month!" };
         private int counter = 0;
         private bool isSelected = false;
-        private string currentSelected = "";
+        public string currentSelected = string.Empty;
+
+        private ConsoleKey pressedKey;
 
         public void UserMenu()
         {
@@ -20,7 +23,9 @@ namespace WellnessBingo
             {
                 Console.SetCursorPosition(left, top);
                 RenderMenu();
+                SetConsoleKey();
                 NavigateMenu();
+                SelectElement();
             }
         }
         public void RenderMenu()
@@ -43,8 +48,7 @@ namespace WellnessBingo
 
         public void NavigateMenu()
         {
-            ConsoleKeyInfo cki = Console.ReadKey(true);
-            switch (cki.Key)
+            switch (pressedKey)
             {
                 case ConsoleKey.DownArrow:
                     if (counter == elements.Length - 1)
@@ -68,11 +72,21 @@ namespace WellnessBingo
                     break;
             }
         }
-
-
         private void UpdateCounter(int arg)
         {
             counter = arg;
+        }
+
+        public void SetConsoleKey() {
+            ConsoleKeyInfo cki = Console.ReadKey(true);
+            pressedKey = cki.Key;
+        }
+
+        public void SelectElement() {
+            if(pressedKey == ConsoleKey.Enter) {
+                currentSelected = elements[counter];
+                isSelected = true;
+            }
         }
         private void Navigate(string nav)
         {
--- WellnessBingo/Program.cs
+++ WellnessBingo/Program.cs
@@ -2,26 +2,17 @@
 {
     internal class Program
     {
-        static string AppName = "WellnessBingo";
-        static string[] december = { "Go ice skating", "Dance to holiday music", "Let go of an old habit", "Create homemade decorations", "Track your water intake", "Build a snowman", "Do a random act of kindness", "Get 10,000 steps in a day", "Bake cookies", "Mail someone a letter", "Practice breathing exercises", "Shop at a small business", "Snuggle in a blanket", "Write a resolution for 2024", "Sit by a fire", "Get yourself a small gift", "Read a chapter of a book", "Volunteer your time", "Be present", "Make a gingerbread house", "Journal your feelings", "Watch a holiday movie", "Have a cozy day" };
-        static string[] november = { "Clean an area of clutter", "Make a pot of soup", "Visit your local library", "Create a vision board", "Write down 3 things you're grateful for", "Bake a pie", "Host a game night", "Go on a scenic drive", "Compliment a stranger", "Enjoy a holiday beverage", "Jump in a pile of leaves", "Go to a farmer's market", "Slow down and enjoy the moment", "Wear fuzzy socks", "Give yourself a hug", "Make someone laugh", "Don't sweat the small stuff", "Take photos in nature", "Have a bonfire", "Don't compare yourself to others", "Take 5 deep breaths", "Spend time with animals", "Try a new skill or balance", "Enjoy a cozy night in" };
-        static string[] october = { "Light (or buy) a candle", "Enjoy a cozy night in", "Write down what you're grateful for", "Call a loved one", "Take a scenic hike", "Go to a farmer's market", "Don't sweat the small stuff", "Go apple picking", "Host a game night", "Compliment a stranger", "Take 5 deep breaths", "Do a meditation outdoors", "Carve a pumpkin", "Give yourself a hug", "Bake a pie", "Declutter your closet", "Enjoy a fall beverage", "Host a book club", "Listen to the campfire sleep sound", "Watch a classic fall movie", "Make someone laugh", "Jump in a pile of leaves", "Visit a pumpkin patch", "Go on a picnic" };
-        static string[] september = { "Attend a local event", "Have a picnic", "Do a random act of kindness", "Work out with a friend", "Meal prep for the week", "Get an extra hour of sleep", "Have a social media-free day", "Stretch", "Get 10,000 steps in a day", "Write out your priorities", "Get some sunshine", "Clean your home", "Practice saying affirmations", "Listen to a podcast", "Hike with a friend", "Start a gratitude journal", "Do your skincare routine", "Check in with family", "Cook a new recipe", "Challenge negative thoughts", "Listen to music", "Light a candle", "Set an intention", "Be proud of yourself" };
-        static string[] july = { "Eat your favorite dessert", "Have a picnic", "Try a new exercise", "Get some sunshine", "Create a summer bucket list", "Volunteer your time", "Stargaze", "Go to a farmer's market", "Take a bike ride", "Treat yourself to ice cream", "Do an art project", "Take a walk on a new trail", "Make a vision board", "Take a day trip to a beach", "Buy yourself flowers", "Clean your home", "Meditate for 10 minutes", "Relax in a hammock", "Host a BBQ with loved ones", "Take a digital detox day", "Start a beach read", "Say 'I love you' to yourself", "Do one thing you've been putting off", "Get 10,000 steps in a day" };
-        static string[] june = { "Plan a road trip", "Build a relaxing evening routine", "Read a chapter in your book", "Meditate for 10 minutes before bed", "Wash your bedding and sheets", "Soak your feet in an Epsom salt bath", "Take 5 deep breaths", "Drink chamomile tea before bed", "Try a new sleep journey", "Enjoy a cozy night in", "Set your thermostat between 60-67°F", "Get 8 hours of sleep", "Make your bed", "Wear your favorite pajamas", "Diffuse lavender oil", "Create a 'wind down' playlist", "Treat yourself to a new pillow or blanket", "Do some gentle stretching or yoga", "Relax in a park", "Go to bed early", "Have a phone-free evening", "Do your skincare routine", "Journal before bed", "Get some morning sunshine" };
-        static string[] nationalWellnessMonth = { "Light a candle", "Treat yourself to a delicious meal", "Go for a nature walk", "Have a 'no phone' day", "Read a chapter of a book", "Do a crossword puzzle", "Spend time in the sun", "Do your skincare routine", "Spend the day at the park", "Set an intention", "Listen to an inspirational podcast", "Write a letter to your future self", "Go to bed early", "Buy yourself a coffee or tea", "Try a new exercise", "Look at pictures of baby animals", "Do an art project", "Make yourself laugh", "Say 3 things you love about yourself", "Stretch", "Have a DIY spa day", "Create your own affirmation", "Do a breathing exercise", "Stay in for a night", "Call a friend or loved one" };
-
         static void Main(string[] args)
         {
-            Console.WriteLine($"Welcome to the {AppName}!");
-            Console.WriteLine("Choose using up and down arrows: ");
             Menu userMenu = new Menu();
             RandomGenerator generator = new RandomGenerator();
-            
-            Console.WriteLine($"Welcome to the app!");
-            Console.WriteLine("Choose using up and down arrows: ");
+            Greet();
             userMenu.UserMenu();
-                
+        }
+
+        static void Greet() {
+            Console.WriteLine($"Welcome to the Welness Bingo!");
+            Console.WriteLine("Choose using up and down arrows: ");
         }
     }
 }
